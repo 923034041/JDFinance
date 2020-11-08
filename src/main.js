2bb7a892647f6e2f745fed57b62e79428a93d748
yinwenjian <923034041@qq.com>
2020-11-08 23:06:33 +0800

    首页模块

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'lib-flexible'
+import './assets/css/reset.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
